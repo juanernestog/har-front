dbbbeb7e16f4af35d6679d7c19a897f57d878b9d
JuanE <juanernestogranados@gmail.com>
2022-06-01 15:43:56 -0500

    Swaped links tag to button in navbar

--- src/components/NavUser.js
+++ src/components/NavUser.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Nav, Button } from 'react-bootstrap';
+//import { Link } from 'react-router-dom';
 
 export default function NavUser() {
   return (
-    <Nav>
-      <Link to="/signup" className="nav-link">
-        Registrarme
-      </Link>
-      <Link to="/login" className="nav-link">
+    <Nav class="nav">
+      {/* TODO: Swapp links to Buttons to the navbar
+      <Nav.Button as={Link} to="/">
+        Inicio
+      </Nav.Button> */}
+      <Button to="/signup" className="nav-link">
+        Registrarme <i class="fas fa-user-plus"></i>
+      </Button>
+      <Button to="/login" className="nav-link">
         Ingresar
-      </Link>
+      </Button>
     </Nav>
   );
 }
