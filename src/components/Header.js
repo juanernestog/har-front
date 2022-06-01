2d3d1082b369a86fa3e5cffc1adc1be5c909def6
JuanE <juanernestogranados@gmail.com>
2022-05-31 21:46:51 -0500

    Swapped css for scss files

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,41 +5,43 @@ import NavUser from './NavUser';
 
 export default function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Link to="/" className="navbar-brand">
-          Harvestify
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Link to="/" className="navbar-brand">
+            Harvestify
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/create" className="nav-link"></Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/">
+            Inicio
+          </Nav.Link>
+        </Nav.Item>
+        <Link to="/signup" className="nav-link">
+          Nosotros
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/create" className="nav-link"></Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/">
-          Inicio
-        </Nav.Link>
-      </Nav.Item>
-      <Link to="/signup" className="nav-link">
-        Nosotros
-      </Link>
-      <Link to="/login" className="nav-link">
-        Contactenos
-      </Link>
-      {/* <Nav.Item>
+        <Link to="/login" className="nav-link">
+          Contactenos
+        </Link>
+        {/* <Nav.Item>
         <Nav.Link as={Link} to="/">
           <i className="fas fa-search" />
         </Nav.Link>
       </Nav.Item> */}
-      <Form>
-        <Form.Group className="mb-3" controlId="SearchBar">
-          <Form.Label>Busqueda</Form.Label>
-          <Form.Control type="email" placeholder="Buscar" />
-        </Form.Group>
-      </Form>
-      <NavUser />
-    </Navbar>
+        <Form>
+          <Form.Group className="mb-3" controlId="SearchBar">
+            <Form.Label>Busqueda</Form.Label>
+            <Form.Control type="email" placeholder="Buscar" />
+          </Form.Group>
+        </Form>
+        <NavUser />
+      </Navbar>
+    </>
   );
 }
