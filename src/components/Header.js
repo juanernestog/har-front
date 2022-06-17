7933d3c23ca3840923a098f2d0749f40ff80c247
Samuel <samuelh08.shm@gmail.com>
2022-06-16 19:50:07 -0500

    removed unused link

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,24 +12,20 @@ export default function Header() {
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/create" className="nav-link"></Link>
-          </Nav>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              Inicio
+            </Nav.Link>
+          </Nav.Item>
+          <Link to="/about" className="nav-link">
+            Nosotros
+          </Link>
+          <Link to="/contact" className="nav-link">
+            Contactenos
+          </Link>
         </Navbar.Collapse>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/">
-            Inicio
-          </Nav.Link>
-        </Nav.Item>
-        <Link to="/about" className="nav-link">
-          Nosotros
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contactenos
-        </Link>
         <Form>
           <Form.Group className="mb-3" controlId="SearchBar">
-            <Form.Label>Busqueda</Form.Label>
             <Form.Control type="product" placeholder="Buscar" />
           </Form.Group>
         </Form>
