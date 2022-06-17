b2f5779c053e7575b82c7ef36e4c5e3b563e011b
JuanE <juanernestogranados@gmail.com>
2022-06-16 19:14:57 -0500

    Burger menu working and aligned items

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,35 +6,31 @@ import NavUser from './NavUser';
 export default function Header() {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
+      <Container class="d-inline-block align-middle">
         <Link to="/" className="navbar-brand">
           Harvestify
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/create" className="nav-link"></Link>
+          <Nav className="me-auto mt-3">
+            <Nav.Link as={Link} to="/">
+              Inicio
+            </Nav.Link>
+            <Link to="/about" className="nav-link">
+              Nosotros
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contactenos
+            </Link>
+            <Form>
+              <Form.Group className="mb-3 nav-search-bar" controlId="SearchBar">
+                <Form.Control type="product" placeholder="Buscar" />
+              </Form.Group>
+            </Form>
           </Nav>
+          <NavUser />
         </Navbar.Collapse>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/">
-            Inicio
-          </Nav.Link>
-        </Nav.Item>
-        <Link to="/about" className="nav-link">
-          Nosotros
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contactenos
-        </Link>
-        <Form>
-          <Form.Group className="mb-3" controlId="SearchBar">
-            <Form.Label>Busqueda</Form.Label>
-            <Form.Control type="product" placeholder="Buscar" />
-          </Form.Group>
-        </Form>
       </Container>
-      <NavUser />
     </Navbar>
   );
 }
