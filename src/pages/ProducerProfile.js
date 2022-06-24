329201afce587604a63fe893e4fe3975c547b6db
Samuel <samuelh08.shm@gmail.com>
2022-06-23 20:09:23 -0500

    implement delete products

--- src/pages/ProducerProfile.js
+++ src/pages/ProducerProfile.js
@@ -1,19 +1,19 @@
 import React, { useContext } from 'react';
-import { Alert, Button, Spinner } from 'react-bootstrap';
+import { Alert, Button, Card, Spinner } from 'react-bootstrap';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import ProductCard from '../components/ProductCard';
+import { deleteProduct } from '../api/products';
 import UserContext from '../containers/UserContext';
-import useProfile from '../hooks/useProfile';
+import useProfile from '../hooks/useProfileProducers';
 
 export default function ProducerProfile() {
   const navigate = useNavigate();
   const { id } = useParams();
   const { user } = useContext(UserContext);
-  const { data, error, loading } = useProfile(id);
+  const { data, error, loading } = useProfile({ id });
 
-  function onDisplayProduct(event, id) {
-    event.stopPropagation();
-    navigate(`products/${id}`);
+  async function removeProduct(event, id) {
+    event.preventDefault();
+    await deleteProduct({ id });
   }
 
   if (loading) {
@@ -36,25 +36,34 @@ export default function ProducerProfile() {
           Editar Perfil
         </Button>
       )}
-      <div className="px-4 py-5 my-5 text-center">
+      <div className="px-4 py-4 my-5 text-center">
         <h1 className="display-5 fw-bold">{data.name}</h1>
-        <div className="col-lg-6 mx-auto">
-          <h2>Mis productos</h2>
+        <div className="mx-auto">
+          <h2>Productos</h2>
           <div className="d-flex flex-wrap justify-content-around">
             {data.products.map((item) => (
-              <div
-                key={item.id}
-                onClick={function (event) {
-                  onDisplayProduct(item.id, event);
-                }}
-              >
-                <ProductCard
-                  producer={item.producer}
-                  name={item.name}
-                  price={item.price}
-                  unit={item.unit}
-                  picture={item.picture}
-                />
+              <div key={item.id}>
+                <Card className="text-center" style={{ width: '10rem' }}>
+                  <Card.Img variant="top" src={item.picture} />
+                  <Card.Body>
+                    <Card.Title>
+                      {item.name}
+                      <br />
+                      <span className="text-muted">
+                        ${item.price} / {item.unit}
+                      </span>
+                    </Card.Title>
+                    <Button variant="primary">Editar</Button>
+                    <Button
+                      variant="danger"
+                      onClick={function (event) {
+                        removeProduct(event, item.id);
+                      }}
+                    >
+                      Eliminar
+                    </Button>
+                  </Card.Body>
+                </Card>
               </div>
             ))}
           </div>
