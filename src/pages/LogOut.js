7df49159f4b594c4008bedb7faadb827db230685
Samuel <samuelh08.shm@gmail.com>
2022-07-03 17:35:51 -0500

    Clear cart context at log out

--- src/pages/LogOut.js
+++ src/pages/LogOut.js
@@ -1,16 +1,19 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CartContext from '../containers/CartContext';
 
 import UserContext from '../containers/UserContext';
 
 export default function LogOut() {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
+  const { setCart } = useContext(CartContext);
 
   useEffect(() => {
     setUser(null);
+    setCart(null);
     navigate('/login');
-  }, [navigate, setUser]);
+  }, [navigate, setUser, setCart]);
 
   return null;
 }
