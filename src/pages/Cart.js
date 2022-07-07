0b2dd8866116ec62c7e422faa955e3cb36d818ac
Samuel <samuelh08.shm@gmail.com>
2022-07-07 16:39:35 -0500

    implemented loading and error in Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Alert, Button, Card, Spinner } from 'react-bootstrap';
 import { deleteCartItem } from '../api/cartItems';
 import { getCart } from '../api/carts';
@@ -6,38 +6,47 @@ import CartContext from '../containers/CartContext';
 
 export default function Cart() {
   const { cart, setCart } = useContext(CartContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const [total, setTotal] = useState(0);
 
-  const calculateTotal = function () {
+  const calculateTotal = useCallback(() => {
     let subtotal = 0;
     cart.cartItems?.map(
       (item) => (subtotal += item.quantity * item.product.price),
     );
     setTotal(subtotal);
-  };
+  }, [cart.cartItems]);
 
   useEffect(() => {
     calculateTotal();
-  }, []);
+  }, [calculateTotal]);
 
   async function removeCartItem(event, id) {
     event.preventDefault();
-    await deleteCartItem({ id });
-    const response = await getCart({ id: cart.id });
-    setCart(response.data);
+    setLoading(true);
+    try {
+      await deleteCartItem({ id });
+      const response = await getCart({ id: cart.id });
+      setCart(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
   }
 
-  // if (loading) {
-  //   return (
-  //     <Spinner animation="border" role="status">
-  //       <span className="visually-hidden">Loading...</span>
-  //     </Spinner>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Cargando...</span>
+      </Spinner>
+    );
+  }
 
   return (
     <>
-      {/* {error && <Alert variant="danger">{error}</Alert>} */}
+      {error && <Alert variant="danger">{error}</Alert>}
       {cart.cartItems?.map((item) => (
         <div key={item.id}>
           <Card className="text-center" style={{ width: '10rem' }}>
@@ -49,15 +58,16 @@ export default function Cart() {
             />
             <Card.Body>
               <Card.Title>
-                {item.name}
+                {item.product.name}
                 <br />
                 <span className="text-muted">
                   ${item.product?.price} / {item.product?.unit}
                 </span>
               </Card.Title>
               <Card.Subtitle className="mb-2 text-muted">
-                Cantidad: {item.quantity}
-                Subtotal: {item.quantity * item.product?.price}
+                <span>Cantidad: {item.quantity}</span>
+                <br />
+                <span>Subtotal: {item.quantity * item.product?.price}</span>
               </Card.Subtitle>
               <Button
                 variant="danger"
