fcb1688896bb049ffe5307b7cf8c8d1e7d650e63
Samuel <samuelh08.shm@gmail.com>
2022-07-15 17:51:34 -0500

    Added payment interaction

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,8 +3,10 @@ import { Alert, Button, Card, Spinner } from 'react-bootstrap';
 import { deleteCartItem } from '../api/cartItems';
 import { getCart } from '../api/carts';
 import CartContext from '../containers/CartContext';
+import UserContext from '../containers/UserContext';
 
 export default function Cart() {
+  const { user } = useContext(UserContext);
   const { cart, setCart } = useContext(CartContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
@@ -36,53 +38,119 @@ export default function Cart() {
     }
   }
 
+  async function pay(event) {
+    event.preventDefault();
+    try {
+      const handler = window.ePayco.checkout.configure({
+        key: process.env.REACT_APP_EPAYCO_PUBLIC_KEY,
+        test: true,
+      });
+      const description = cart.cartItems?.map(
+        (item) => `${item.product.name}X${item.quantity}`,
+      );
+      await handler.open({
+        //Parametros compra (obligatorio)
+        name: 'Compra',
+        description: description.join(', '),
+        invoice: cart.id,
+        currency: 'cop',
+        amount: total,
+        tax_base: '0',
+        tax: '0',
+        country: 'co',
+        lang: 'es',
+
+        //Onpage="false" - Standard="true"
+        external: 'false',
+
+        //Atributos opcionales
+        confirmation: 'http://secure2.payco.co/prueba_curl.php',
+        response: `${process.env.REACT_APP_SERVER_URL}/response`,
+
+        //Atributos cliente
+        name_billing: user.name,
+        address_billing: cart.address,
+        mobilephone_billing: user.tel,
+        type_doc_billing: '',
+        number_doc_billing: '',
+
+        //atributo deshabilitación metodo de pago
+        methodsDisable: ['CASH'],
+      });
+    } catch (error) {
+      setError(error);
+    }
+  }
+
   if (loading) {
     return (
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Cargando...</span>
-      </Spinner>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Spinner animation="border" role="status"></Spinner>
+      </div>
     );
   }
 
   return (
     <>
       {error && <Alert variant="danger">{error}</Alert>}
-      {cart.cartItems?.map((item) => (
-        <div key={item.id}>
-          <Card className="text-center" style={{ width: '10rem' }}>
-            <Card.Img
-              style={{ objectFit: 'contain' }}
-              height="100px"
-              variant="top"
-              src={item.product?.picture.path}
-            />
-            <Card.Body>
-              <Card.Title>
-                {item.product.name}
-                <br />
-                <span className="text-muted">
-                  ${item.product?.price} / {item.product?.unit}
-                </span>
-              </Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                <span>Cantidad: {item.quantity}</span>
-                <br />
-                <span>Subtotal: {item.quantity * item.product?.price}</span>
-              </Card.Subtitle>
-              <Button
-                variant="danger"
-                onClick={function (event) {
-                  removeCartItem(event, item.id);
-                }}
-              >
-                Eliminar
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
-      <div>Total: {total}</div>
-      <Button>Pagar</Button>
+      <div className="mt-5">
+        {cart.cartItems?.map((item) => (
+          <div key={item.id}>
+            <Card
+              className="text-center flex-row align-items-center"
+              style={{ width: '20rem' }}
+            >
+              <Card.Img
+                style={{ objectFit: 'contain' }}
+                height="100px"
+                variant="left"
+                src={item.product?.picture.path}
+              />
+              <Card.Body>
+                <Card.Title>
+                  {item.product.name}
+                  <br />
+                  <span className="text-muted">
+                    ${item.product?.price} / {item.product?.unit}
+                  </span>
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  <span>Cantidad: {item.quantity}</span>
+                  <br />
+                  <span>Subtotal: ${item.quantity * item.product?.price}</span>
+                </Card.Subtitle>
+                <Button
+                  variant="danger"
+                  onClick={function (event) {
+                    removeCartItem(event, item.id);
+                  }}
+                >
+                  Eliminar
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+      <div>Total: ${total}</div>
+      <Button
+        variant="primary"
+        onClick={function (event) {
+          pay(event);
+        }}
+      >
+        Pagar
+      </Button>
     </>
   );
 }
