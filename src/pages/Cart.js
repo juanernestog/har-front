ddaa354c7bbc22d6d6bf164f1fc7cf1f1c37f016
Samuel <samuelh08.shm@gmail.com>
2022-07-26 23:59:45 -0500

    renamed env variable as base_url

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -65,7 +65,7 @@ export default function Cart() {
 
         //Atributos opcionales
         confirmation: 'http://secure2.payco.co/prueba_curl.php',
-        response: `${process.env.REACT_APP_URL}/payment-response`,
+        response: `${process.env.REACT_APP_BASE_URL}/payment-response`,
 
         //Atributos cliente
         name_billing: user.name,
